369417e6f724e11367dea356cd4478d9b2e2d02c
patrick <patrick.deklein@cito.nl>
2023-07-10 11:26:15 +0200

    created a barrel

--- src/lib/qti-components/qti-responseprocessing/index.ts
+++ src/lib/qti-components/qti-responseprocessing/index.ts
@@ -1,5 +1,6 @@
 export { default as QtiResponseProcessing } from './qti-responseprocessing/qti-responseprocessing';
 export { QtiRule } from './qti-rule/qti-rule';
+
 export * from './qti-rule/qti-responsecondition/qti-responsecondition';
 export * from './qti-rule/qti-setoutcomevalue/qti-setoutcomevalue';
 
@@ -7,26 +8,4 @@ export * from './qti-responseelse/qti-responseif/qti-responseif';
 export * from './qti-responseelse/qti-responseif/qti-responseelseif/qti-responseelseif';
 export * from './qti-responseelse/qti-responseelse';
 
-export * from './qti-expression/qti-gt/qti-gt';
-export * from './qti-expression/qti-gte/qti-gte';
-export * from './qti-expression/qti-lt/qti-lt';
-export * from './qti-expression/qti-lte/qti-lte';
-export * from './qti-expression/qti-and/qti-and';
-export * from './qti-expression/qti-product/qti-product';
-export * from './qti-expression/qti-or/qti-or';
-export * from './qti-expression/qti-equal/qti-equal';
-export * from './qti-expression/qti-equal-rounded/qti-equal-rounded';
-export * from './qti-expression/qti-basevalue/qti-basevalue';
-export * from './qti-expression/qti-contains/qti-contains';
-export * from './qti-expression/qti-expression';
-export * from './qti-expression/qti-correct/qti-correct';
-export * from './qti-expression/qti-is-null/qti-is-null';
-export * from './qti-expression/qti-not/qti-not';
-export * from './qti-expression/qti-mapresponse/qti-mapresponse';
-export * from './qti-expression/qti-match/qti-match';
-export * from './qti-expression/qti-variable/qti-variable';
-export * from './qti-expression/qti-multiple/qti-multiple';
-export * from './qti-expression/qti-ordered/qti-ordered';
-export * from './qti-expression/qti-printed-variable/qti-printed-variable';
-export * from './qti-expression/qti-member/qti-member';
-export * from './qti-expression/qti-mapping/qti-mapping';
+export * from './qti-expression';
--- src/lib/qti-components/qti-responseprocessing/qti-expression/qti-condition-expression.ts
+++ src/lib/qti-components/qti-responseprocessing/qti-expression/qti-condition-expression.ts
@@ -1,7 +1,7 @@
 import { html } from 'lit';
 import { QtiExpression } from './qti-expression';
 
-export class QtiConditionExpression extends QtiExpression<boolean> {
+export abstract class QtiConditionExpression extends QtiExpression<boolean> {
   public calculate(): boolean {
     // eslint-disable-next-line no-throw-literal
     throw new Error('Not implemented');
