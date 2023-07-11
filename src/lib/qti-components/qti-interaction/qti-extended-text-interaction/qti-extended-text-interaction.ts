f55533474d12c803ad32c65484a015b6541a371d
patrick <patrick.deklein@cito.nl>
2023-07-11 09:52:47 +0200

    fitted textarea

--- src/lib/qti-components/qti-interaction/qti-extended-text-interaction/qti-extended-text-interaction.ts
+++ src/lib/qti-components/qti-interaction/qti-extended-text-interaction/qti-extended-text-interaction.ts
@@ -52,6 +52,7 @@ export class QtiExtendedTextInteraction extends Interaction {
           display: block;
         }
         textarea {
+          box-sizing: border-box;
           width: 100%;
           height: 100%;
           border: 0;
