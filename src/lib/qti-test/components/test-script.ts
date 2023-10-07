2e2e513b375624524379442e900a3f1cb04d90b1
patrick <patrick.deklein@cito.nl>
2023-10-07 14:40:29 +0200

    whoo print variables

--- src/lib/qti-test/components/test-script.ts
+++ src/lib/qti-test/components/test-script.ts
@@ -1,6 +1,6 @@
 import { ContextConsumer } from '@lit-labs/context';
 import { html, LitElement } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, state } from 'lit/decorators.js';
 
 import { testContext } from '../qti-assessment-test.context';
 
@@ -9,18 +9,27 @@ export class TestScript extends LitElement {
   private operatorFunction: Function;
   private _context: ContextConsumer<any, this>;
 
+  @state()
+  private _printed: string = '';
+
   render() {
-    return html`<slot @slotchange=${this.handleSlotChange}></slot>`;
+    return html`${this._printed}<slot @slotchange=${this.handleSlotChange}></slot>`;
   }
 
   handleSlotChange(event: Event) {
-    // expecting <[!CDATA[ ... ]]> is converted into <!-- ... --> with qti-transform: cDataToComment
     const commentNode = Array.from(this.childNodes ?? []).find(node => node.nodeType === Node.COMMENT_NODE);
     try {
       this.operatorFunction = new Function('context', commentNode.textContent ?? '');
     } catch (e) {
       console.error('custom-operator contains invalid javascript code', e);
     }
-    this._context = new ContextConsumer(this, testContext, e => this.operatorFunction(e), true);
+    this._context = new ContextConsumer(
+      this,
+      testContext,
+      e => {
+        this._printed = this.operatorFunction(e);
+      },
+      true
+    );
   }
 }
