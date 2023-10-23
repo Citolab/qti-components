7af48921da5a1e5f33dcd332671ae345cb9887ec
Marcel Hoekstra <marcel_hoekstra@outlook.com>
2023-10-23 13:18:43 +0200

    added restoreContext

--- src/lib/qti-test/qti-assessment-test.ts
+++ src/lib/qti-test/qti-assessment-test.ts
@@ -4,39 +4,47 @@ import { QtiAssessmentItem } from '../qti-components';
 import { provide } from '@lit/context';
 import { TestContext, testContext } from './qti-assessment-test.context';
 import { QtiAssessmentItemRef } from './qti-assessment-item-ref';
-import { val } from 'cheerio/lib/api/attributes';
 
 @customElement('qti-assessment-test')
 export class QtiAssessmentTest extends LitElement {
-  private _context: TestContext = { itemIndex: null, items: [] }; // <--- is null when no item is loaded, can be used to show a loader icon
+  private _initialValue: TestContext = {
+    itemIndex: null, // <--- is null when no item is loaded, can be used to show a loader icon
+    items: []
+  };
 
   @provide({ context: testContext })
   @property({ attribute: false })
-  set context(value: TestContext) {
-    this._context.items.forEach(itemContext => {
-      if (!value.items.find(i => i.identifier === itemContext.identifier)) {
-        value.items.push(itemContext);
-      }
-    });
-    this._context = value;
-  }
-  get context(): TestContext {
-    return this._context;
-  }
+  public context: TestContext = this._initialValue;
 
   itemRefEls: Map<string, QtiAssessmentItemRef> = new Map();
 
   // only copies the variables from the item, back into the testcontext to retain state
   private copyItemVariables(identifier: string): void {
-    this._context = {
-      ...this._context,
-      items: this._context.items.map(itemContext => {
+    this.context = {
+      ...this.context,
+      items: this.context.items.map(itemContext => {
         return itemContext.identifier == identifier
           ? { ...itemContext, variables: this.getAssessmentItem(identifier).context.variables }
           : itemContext;
       })
     };
   }
+
+  restoreContext = (contextToRestore: TestContext) => {
+    if (!contextToRestore) {
+      contextToRestore = this._initialValue;
+    }
+    this.context.itemIndex = contextToRestore.itemIndex;
+    // append the items that are not yet in the context and replace the ones that are
+    contextToRestore.items?.forEach(itemContext => {
+      const existingItemContext = this.context.items.find(i => i.identifier === itemContext.identifier);
+      if (existingItemContext) {
+        existingItemContext.variables = itemContext.variables;
+      } else {
+        this.context.items.push(itemContext);
+      }
+    });
+  };
 
   getAssessmentItem(identifier: string): QtiAssessmentItem {
     return this.querySelector<QtiAssessmentItemRef>(`qti-assessment-item-ref[identifier="${identifier}"]`)
@@ -47,8 +55,8 @@ export class QtiAssessmentTest extends LitElement {
     this.itemRefEls.set(e.detail.identifier, e.target as QtiAssessmentItemRef);
 
     const { href, identifier } = e.detail;
-    this._context.items = [
-      ...this._context.items,
+    this.context.items = [
+      ...this.context.items,
       {
         href,
         identifier,
@@ -67,23 +75,23 @@ export class QtiAssessmentTest extends LitElement {
 
   private onTestRequestItem(e: CustomEvent<number>): void {
     e.stopImmediatePropagation();
-    if (e.detail === this._context.itemIndex) return; // same item
+    if (e.detail === this.context.itemIndex) return; // same item
 
     // PK: this is where the magic should happen
     // can or can we not navigate to the next item?
     // if we can navigate to the next item, then should we?
 
     // - processResponse?, not for now, it will give feedback when we don't want to
-    // this._context.items[this._context.itemIndex]?.itemEl.processResponse();
+    // this.context.items[this.context.itemIndex]?.itemEl.processResponse();
 
     const truthy = true;
-    if (truthy) {
+    if (!truthy) {
       // - set the index to id we want it to be
-      this._context = { ...this._context, itemIndex: e.detail };
+      this.context = { ...this.context, itemIndex: e.detail };
     } else {
       // - set the index to null, meaning we finished this item and testContext will be triggered
-      this._context = { ...this._context, itemIndex: null };
-      this._requestItem(this._context.items[e.detail].identifier);
+      this.context = { ...this.context, itemIndex: null };
+      this._requestItem(this.context.items[e.detail].identifier);
     }
 
     // - request a new item to the outer realm!
@@ -91,15 +99,18 @@ export class QtiAssessmentTest extends LitElement {
 
   firstUpdated(a): void {
     super.firstUpdated(a);
-    if (this._context.items.length === 0) return;
-    this._requestItem(this._context.items[0].identifier);
+    if (this.context.items.length === 0) {
+      console.warn('No items found in the test, please add at least one item');
+      return;
+    }
+    this._requestItem(this.context.items[0].identifier);
   }
 
   updated(changedProperties: Map<string, any>) {
     if (changedProperties.has('context')) {
       const oldIndex = changedProperties.get('context')?.itemIndex;
-      if (this._context.itemIndex !== null && oldIndex !== this._context.itemIndex) {
-        this._requestItem(this._context.items[this._context.itemIndex].identifier);
+      if (this.context.itemIndex !== null && oldIndex !== this.context.itemIndex) {
+        this._requestItem(this.context.items[this.context.itemIndex].identifier);
       }
     }
   }
@@ -117,13 +128,13 @@ export class QtiAssessmentTest extends LitElement {
     // Ah, the new item has entered the inner realm, let's connect it to the context
     // get the index of the current item
     // debugger;
-    const itemIndex = this._context.items.findIndex(itemContext => itemContext.identifier === item.identifier);
+    const itemIndex = this.context.items.findIndex(itemContext => itemContext.identifier === item.identifier);
 
     // set the index of the current item, triggering the context ( set disabled or not )
-    this._context = { ...this._context, itemIndex };
+    this.context = { ...this.context, itemIndex };
 
     // get the testcontext of this item
-    const itemContext = this._context.items.find(i => i?.identifier === item?.identifier);
+    const itemContext = this.context.items.find(i => i?.identifier === item?.identifier);
     // if it is still empty, then copy the variables from the item
     if (itemContext.variables.length === 1) {
       this.copyItemVariables(item.identifier);
@@ -147,3 +158,19 @@ export class QtiAssessmentTest extends LitElement {
     return html`<slot></slot>`;
   }
 }
+
+// set context(value: TestContext) {
+//   // if (!value) {
+//   //   value = this._initialValue;
+//   // }
+//   // this._context.items?.forEach(itemContext => {
+//   //   if (!value.items) value.items = [];
+//   //   if (!value.items.find(i => i.identifier === itemContext.identifier)) {
+//   //     value.items.push(itemContext);
+//   //   }
+//   // });
+//   // this._context = value;
+// }
+// get context(): TestContext {
+//   return this._context;
+// }
