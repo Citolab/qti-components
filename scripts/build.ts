faf7c23a169a2875bfe3e2d7153b43fb5396bb35
patrick <patrick.deklein@cito.nl>
2023-11-01 13:35:31 +0100

    sync builds with extended

--- scripts/build.ts
+++ scripts/build.ts
@@ -8,31 +8,40 @@ console.log('Building the project...');
 const outdir = 'dist';
 
 (async () => {
-  const targetsResult = await tsup.build({
-    target: 'es2017',
-    dts: true,
-    format: ['esm', 'cjs'],
-    clean: true,
-    minify: true,
-    bundle: true,
-    entryPoints: [
-      // NOTE: Entry points must be mapped in package.json > exports, otherwise users won't be able to import them!
-      './src/lib/context/index.ts',
-      './src/lib/decorators/index.ts',
-      './src/lib/qti-components/index.ts',
-      './src/lib/qti-item/index.ts',
-      './src/lib/qti-item-react/index.ts',
-      './src/lib/qti-transform/index.ts',
-      './src/lib/qti-test/index.ts',
-      './src/lib/qti-test-react/index.ts'
-    ],
-    define: {
-      'process.env.NODE_ENV': '"production"'
-    },
-    external: ['@lit/react', '@lit/context', 'react', 'lit'],
-    splitting: true,
-    esbuildPlugins: [InlineCSSPlugin] // https://github.com/evanw/esbuild/issues/3109#issuecomment-1539846087
-  });
+  await tsup
+    .build({
+      target: 'es2017',
+      dts: true,
+      format: ['esm', 'cjs'],
+      clean: true,
+      minify: true,
+      bundle: true,
+      entryPoints: [
+        //
+        // NOTE: Entry points must be mapped in package.json > exports, otherwise users won't be able to import them!
+        //
+        './src/lib/context/index.ts',
+        './src/lib/decorators/index.ts',
+        './src/lib/qti-components/index.ts',
+        './src/lib/qti-item/index.ts',
+        './src/lib/qti-item-react/index.ts',
+        './src/lib/qti-transform/index.ts',
+        './src/lib/qti-test/index.ts',
+        './src/lib/qti-test-react/index.ts'
+      ],
+      define: {
+        'process.env.NODE_ENV': '"production"'
+      },
+      external: ['@lit/react', '@lit/context', 'react', 'lit'],
+      splitting: true,
+      esbuildPlugins: [InlineCSSPlugin] // https://github.com/evanw/esbuild/issues/3109#issuecomment-1539846087
+    })
+    .catch(err => {
+      console.error(chalk.red(err));
+      process.exit(1);
+    })
+    .then(result => console.log(result));
+
   console.log(chalk.green(`qti-components has been generated at ${outdir}\n`));
 
   try {
