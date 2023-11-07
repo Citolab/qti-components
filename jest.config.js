26ac1c5d7209c37cebbb93d93cd851030dafbe0b
Marcel Hoekstra <marcel_hoekstra@outlook.com>
2023-11-07 10:24:56 +0100

    another try to make tests work

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,21 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig';
 export default {
   // preset: "ts-jest",
   verbose: true,
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
   transform: {
-    "^.+\\.(js|ts)$": ["ts-jest", {
-      tsconfig: {
-        allowJs: true,
-      },
-    }]
+    '^.+\\.(js|ts)$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          allowJs: true
+        }
+      }
+    ]
   },
-  transformIgnorePatterns: ["node_modules/(?!@?lit)"],
+  modulePaths: [compilerOptions.baseUrl],
+  transformIgnorePatterns: ['node_modules/(?!@?lit)'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths /*, { prefix: '<rootDir>/' } */)
   // modulePathIgnorePatterns: ['node_modules', '.jest-test-results.json']
-}
+};
