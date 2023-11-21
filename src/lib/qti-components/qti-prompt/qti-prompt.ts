b6d8b806c4cfcc28880892aa2fa8706e5c6f2943
patrick <patrick.deklein@cito.nl>
2023-11-21 18:51:49 +0100

    pre added .js to decorator

--- src/lib/qti-components/qti-prompt/qti-prompt.ts
+++ src/lib/qti-components/qti-prompt/qti-prompt.ts
@@ -1,5 +1,5 @@
 import { html, LitElement } from 'lit';
-import { customElement } from 'lit/decorators';
+import { customElement } from 'lit/decorators.js';
 @customElement('qti-prompt')
 export class QtiPrompt extends LitElement {
   override render() {
