f733033d38909048e0b9a7ba1c0e620eb169a7fe
patrick <patrick.deklein@cito.nl>
2024-02-08 23:55:29 +0100

    try vitest browser

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [tsconfigPaths.default()],
+  base: process.env.VITEST ? undefined : './src',
   test: {
     include: ['src/**/*.spec.ts'],
     globals: true,
