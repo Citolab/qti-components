5cb9ccf74938ef5e6d188899d501ff39169c391c
Marcel Hoekstra <marcel_hoekstra@outlook.com>
2024-07-26 08:59:41 +0200

    injected scripting in bootstrap js en index.html of ci

--- public/assets/qti-custom-interaction/ref/6047-BiKB-bmi_467x370_29/script/bootstrap.js
+++ public/assets/qti-custom-interaction/ref/6047-BiKB-bmi_467x370_29/script/bootstrap.js
@@ -3,59 +3,19 @@
 // is embedded in an iframe and coming from another domain
 // Therefor we need to use the new CES API to communicates via the broadcast API
 
-window.CES = {
-  media: null,
-  response: null,
-  load: () => {
-    const channel = new BroadcastChannel('ces_channel');
-
-    return new Promise((resolve, reject) => {
-      let resolveCount = 0;
-      const handleMessage = event => {
-        if (event.data.type === 'mediaData') {
-          const media = event.data.data;
-          CES.media = media;
-          resolveCount++;
-        } else if (event.data.type === 'responseData') {
-          const response = event.data.data;
-          CES.response = response;
-          resolveCount++;
-        }
-        if (resolveCount === 2) {
-          channel.close();
-          return resolve();
-        }
-      };
-
-      channel.onmessage = handleMessage;
-      channel.postMessage({ type: 'getMedia' });
-      channel.postMessage({ type: 'getResponse' });
-    });
-  },
-  setResponse: data => {
-    const channel = new BroadcastChannel('ces_channel');
-    channel.postMessage({ type: 'setResponse', data });
-    channel.close();
-  },
-  getResponse: () => {
-    return CES.response;
-  },
-  getMedia: () => {
-    return CES.media;
-  },
-  setStageHeight: () => {
-    const channel = new BroadcastChannel('ces_channel');
-    channel.postMessage({ type: 'setStageHeight' });
-    channel.close();
-  }
-};
-
 window.onload = async function () {
-  await CES.load();
-
-  var n = document.createElement('iframe');
-  n.frameBorder = '0';
-  n.scrolling = 'no';
-  n.src = CES.getMedia()[0];
-  document.body.appendChild(n);
+  const channel = new BroadcastChannel('ces_channel');
+  const handleMessage = event => {
+    if (event.data.type === 'mediaData') {
+      const media = event.data.data;
+      var n = document.createElement('iframe');
+      n.frameBorder = '0';
+      n.scrolling = 'no';
+      n.src = media[0];
+      document.body.appendChild(n);
+      channel.close();
+    }
+  };
+  channel.onmessage = handleMessage;
+  channel.postMessage({ type: 'getMedia' });
 };
