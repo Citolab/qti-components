2bdaf14a751d9c3db05163f90da509380766c8cd
Marcel Hoekstra <marcel_hoekstra@outlook.com>
2024-08-20 12:52:34 +0200

    pass a copy of the response

--- src/lib/qti-components/qti-interaction/qti-match-interaction/qti-match-interaction.ts
+++ src/lib/qti-components/qti-interaction/qti-match-interaction/qti-match-interaction.ts
@@ -48,33 +48,32 @@ export class QtiMatchInteraction extends DragDropInteractionMixin(
     this.response = [];
   }
 
-  handleRadioClick = e => {    
+  handleRadioClick = e => {
     const radio = e.target as HTMLInputElement;
-    if (this.lastCheckedRadio === radio) {      
+    if (this.lastCheckedRadio === radio) {
       radio.checked = false;
       this.lastCheckedRadio = null;
       this.handleRadioChange(e);
-    } else {      
+    } else {
       this.lastCheckedRadio = radio;
     }
   };
 
-  handleRadioChange = e => {    
+  handleRadioChange = e => {
     const checkbox = e.target as HTMLInputElement;
     const value = checkbox.value;
-    const name = checkbox.name;    
+    const name = checkbox.name;
     const type = checkbox.type;
-    
+
     if (checkbox.checked) {
       if (!this.response) {
         this.response = [value];
-      }
-      else if (this.response.indexOf(value) === -1) {    
+      } else if (this.response.indexOf(value) === -1) {
         if (type === 'radio') {
           this.response = this.response.filter(v => v.indexOf(name) === -1);
-        }   
+        }
         this.response = [...this.response, value];
-      }      
+      }
       this.lastCheckedRadio = checkbox;
     } else {
       this.response = this.response.filter(v => v !== value);
@@ -88,14 +87,13 @@ export class QtiMatchInteraction extends DragDropInteractionMixin(
         composed: true,
         detail: {
           responseIdentifier: this.responseIdentifier,
-          response: this.response
+          response: Array.isArray(this.response) ? [...this.response] : this.response
         }
       })
     );
   };
 
-
-  set correctResponse(responseValue: string | string[]) {    
+  set correctResponse(responseValue: string | string[]) {
     if (responseValue === '') {
       this.correctOptions = [];
       return;
@@ -125,15 +123,20 @@ export class QtiMatchInteraction extends DragDropInteractionMixin(
                 const colId = col.getAttribute('identifier');
                 const value = `${rowId} ${colId}`;
                 const selectedInRowCount = this.response.filter(v => v.split(' ')[0] === rowId).length || 0;
-                const checked = this.response.includes(value);                
+                const checked = this.response.includes(value);
                 const part = `rb ${checked ? 'rb-checked' : ''} ${this.correctOptions.includes(value) ? 'rb-correct' : ''}`;
                 // disable if match max is greater than 1 and max is reached
-                const disable = this.correctOptions.length > 0 ? true : row.matchMax === 1 ? false : selectedInRowCount >= row.matchMax && !checked;                
+                const disable =
+                  this.correctOptions.length > 0
+                    ? true
+                    : row.matchMax === 1
+                      ? false
+                      : selectedInRowCount >= row.matchMax && !checked;
                 return html`<td>
                   <input
-                    type=${row.matchMax === 1 ? 'radio' : `checkbox`}                    
+                    type=${row.matchMax === 1 ? 'radio' : `checkbox`}
                     role="id"
-                    part=${part}                    
+                    part=${part}
                     name=${rowId}
                     value=${value}
                     .disabled=${disable}
@@ -148,7 +151,6 @@ export class QtiMatchInteraction extends DragDropInteractionMixin(
     `;
   }
 }
-
 
 declare global {
   interface HTMLElementTagNameMap {
