b29d3d8607dad55aea0950e03e22a201e2a8379b
Marcel Hoekstra <marcel_hoekstra@outlook.com>
2024-08-20 13:46:12 +0200

    extra story with matrix and response processing

--- src/lib/qti-components/qti-interaction/qti-match-interaction/qti-match-interaction.stories.ts
+++ src/lib/qti-components/qti-interaction/qti-match-interaction/qti-match-interaction.stories.ts
@@ -2,6 +2,8 @@ import { action } from '@storybook/addon-actions';
 import { html } from 'lit';
 
 import '../../index';
+import { QtiAssessmentItem } from '../../index';
+import { createRef, ref } from 'lit/directives/ref.js';
 
 export default {
   component: 'qti-match-interaction'
@@ -85,6 +87,112 @@ export const Tabular = {
         template="https://purl.imsglobal.org/spec/qti/v3p0/rptemplates/map_response.xml"
       ></qti-response-processing>
     </qti-assessment-item>`
+};
+
+export const TabularAardrijkskunde = {
+  render: () => {
+    const testRef = createRef<QtiAssessmentItem>();
+    return html`<qti-assessment-item
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://www.imsglobal.org/xsd/imsqtiasi_v3p0 https://purl.imsglobal.org/spec/qti/v3p0/schema/xsd/imsqti_asiv3p0_v1p0.xsd
+						http://www.duo.nl/schema/dep_extension ../dep_extension.xsd"
+        title="3297cq"
+        identifier="ITM-3297cq"
+        time-dependent="false"
+        label="3297cq"
+         ${ref(testRef)}
+              @qti-interaction-changed="${e => {
+                testRef.value.processResponse();
+                action(JSON.stringify(e.detail))();
+              }}"
+      @qti-outcome-changed="${e => {
+        action(JSON.stringify(e.detail))();
+      }}"
+        xmlns="http://www.imsglobal.org/xsd/imsqtiasi_v3p0"
+        xmlns:dep="http://www.duo.nl/schema/dep_extension"
+      >
+        <qti-response-declaration identifier="RESPONSE" cardinality="multiple" base-type="directedPair">
+          <qti-correct-response interpretation="A&amp;B">
+            <qti-value>y_A x_1</qti-value>
+            <qti-value>y_B x_2</qti-value>
+          </qti-correct-response>
+        </qti-response-declaration>
+        <qti-outcome-declaration identifier="SCORE" cardinality="single" base-type="float">
+          <qti-default-value>
+            <qti-value>0</qti-value>
+          </qti-default-value>
+        </qti-outcome-declaration>
+        <qti-outcome-declaration identifier="MAXSCORE" cardinality="single" base-type="float">
+          <qti-default-value>
+            <qti-value>1</qti-value>
+          </qti-default-value> </qti-outcome-declaration
+        >
+        <qti-item-body class="defaultBody" xml:lang="nl-NL">
+          <qti-match-interaction
+          id="matchInteraction1"
+          class="qti-match-tabular "
+          min-associations="2"
+          max-associations="2"
+          shuffle="false"
+          response-identifier="RESPONSE"
+        >
+          <qti-simple-match-set>
+            <qti-simple-associable-choice identifier="y_A" match-max="1">
+              <div>
+                <p>Als koele lucht botst tegen warme vochtige lucht ontstaat frontale neerslag.</p>
+              </div>
+            </qti-simple-associable-choice>
+
+            <qti-simple-associable-choice identifier="y_B" match-max="1">
+              <div>
+                <p>De aanvoer van koude lucht vanuit het noorden wordt door de ligging van gebergtes tegengehouden.</p>
+              </div>
+            </qti-simple-associable-choice>
+          </qti-simple-match-set>
+          <qti-simple-match-set>
+            <qti-simple-associable-choice identifier="x_1" match-max="2">
+              <div class="cito_genclass_BB_2017_A1-012_1">
+                <p>juist</p>
+              </div>
+            </qti-simple-associable-choice>
+
+            <qti-simple-associable-choice identifier="x_2" match-max="2">
+              <div class="cito_genclass_BB_2017_A1-012_2">
+                <p>onjuist</p>
+              </div>
+            </qti-simple-associable-choice>
+          </qti-simple-match-set>
+        </qti-match-interaction>
+
+      	</qti-item-body>
+             <qti-response-processing
+    		template="https://purl.imsglobal.org/spec/qti/v3p0/rptemplates/match_correct.xml"></qti-response-processing>
+    </qti-response-processing>
+    </qti-assessment-item>`;
+
+    //   <qti-response-processing
+    // 		template="https://purl.imsglobal.org/spec/qti/v3p0/rptemplates/match_correct.xml"></qti-response-processing>
+    // </qti-response-processing>
+
+    //   <qti-response-processing xmlns="http://www.imsglobal.org/xsd/imsqtiasi_v3p0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.imsglobal.org/xsd/imsqtiasi_v3p0 https://purl.imsglobal.org/spec/qti/v3p0/schema/xsd/imsqti_asiv3p0_v1p0.xsd">
+    // <qti-response-condition>
+    // <qti-response-if>
+    // <qti-match>
+    // <qti-variable identifier="RESPONSE"/>
+    // <qti-correct identifier="RESPONSE"/>
+    // </qti-match>
+    // <qti-set-outcome-value identifier="SCORE">
+    // <qti-base-value base-type="float">1</qti-base-value>
+    // </qti-set-outcome-value>
+    // </qti-response-if>
+    // <qti-response-else>
+    // <qti-set-outcome-value identifier="SCORE">
+    // <qti-base-value base-type="float">0</qti-base-value>
+    // </qti-set-outcome-value>
+    // </qti-response-else>
+    // </qti-response-condition>
+    // </qti-response-processing>
+  }
 };
 
 export const TabularMultiple = {
