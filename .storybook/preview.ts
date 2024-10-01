4146b56e068a2a4fbe612809933d520f7e7f2549
Marcel Hoekstra <marcel_hoekstra@outlook.com>
2024-10-01 16:33:03 +0200

    try to remove lit comment

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,9 +4,39 @@ import customElements from '../custom-elements.json';
 
 import '../src/index.ts';
 import '../src/item.css';
-
 import { customViewports } from './custom-viewport-sizes';
 setCustomElementsManifest(customElements);
+
+// .storybook/preview.js
+
+export const decorators = [
+  (Story, context) => {
+    const storyElement = Story(context);
+
+    // Cleanup Lit comments after the story is rendered
+    requestAnimationFrame(() => {
+      removeLitComments(document.body);
+    });
+
+    return storyElement;
+  }
+];
+
+// Helper function to remove Lit comments
+function removeLitComments(element) {
+  const treeWalker = document.createTreeWalker(element, NodeFilter.SHOW_COMMENT, {
+    acceptNode(node) {
+      if (node.nodeValue && node.nodeValue.startsWith('lit$')) {
+        return NodeFilter.FILTER_ACCEPT;
+      }
+      return NodeFilter.FILTER_REJECT;
+    }
+  });
+  let currentNode;
+  while ((currentNode = treeWalker.nextNode())) {
+    currentNode.parentNode.removeChild(currentNode);
+  }
+}
 
 const preview: Preview = {
   globalTypes: {
