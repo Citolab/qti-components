9187d5e378b1f702eee73729857f81527d4b1142
Patrick de Klein <patrick.deklein@cito.nl>
2024-11-05 10:47:26 +0100

    some xml transform tests

--- src/lib/qti-transformers/qti-transformers.spec.ts
+++ src/lib/qti-transformers/qti-transformers.spec.ts
@@ -1,56 +1,140 @@
 const xml = String.raw;
 const html = String.raw;
 
-import { qtiTransformItem, qtiTransformTest } from './qti-transformers';
+import { qtiTransformItem } from './qti-transformers';
 
-describe('qtiTransformTest', () => {
-  it('should return an object with the expected methods', async () => {
-    const parsedXML = qtiTransformTest()
-      .parse(
-        xml`
-    <qti-assessment-test>
-      <qti-assessment-item-ref identifier="ITM-choice" href="items/choice.xml" />
-      <qti-assessment-item-ref identifier="ITM-text" href="items/text.xml" />
-    </qti-assessment-test>`
-      )
-      .html();
-
-    expect(parsedXML).toMatch(
-      html`
-<qti-assessment-test xmlns="http://www.w3.org/1999/xhtml">
-      <qti-assessment-item-ref identifier="ITM-choice" href="items/choice.xml"></qti-assessment-item-ref>
-      <qti-assessment-item-ref identifier="ITM-text" href="items/text.xml"></qti-assessment-item-ref>
-    </qti-assessment-test>`
-    );
-  });
-});
-
-describe('qtiTransformItem', () => {
-  it('should return an object with the expected methods', async () => {
+describe('qtiTransformItem API Methods', () => {
+  it('should apply customInteraction correctly', async () => {
     const parsedXML = qtiTransformItem()
       .parse(
         xml`
-    <qti-assessment-item>
+        <qti-assessment-item>
+          <qti-item-body>
+            <qti-custom-interaction>
+              <object data="baseData" width="400" height="300"></object>
+            </qti-custom-interaction>
+          </qti-item-body>
+        </qti-assessment-item>`
+      )
+      .customInteraction('baseRef', 'item123')
+      .html();
+
+    expect(parsedXML).toEqualXml(
+      html`
+      <qti-assessment-item xmlns="http://www.w3.org/1999/xhtml">
         <qti-item-body>
-          <qti-choice-interaction response-identifier="RESPONSE" class="type:custom">
-            <qti-simple-choice identifier="A">Choice A</qti-simple-choice>
-            <qti-simple-choice identifier="B">Choice B</qti-simple-choice>
-          </qti-choice-interaction>
+          <qti-custom-interaction data-base-ref="baseRef" data-base-item="baseRefitem123" data="baseData" width="400" height="300"></qti-custom-interaction>
         </qti-item-body>
-    </qti-assessment-item>`
-      ).customTypes()
+      </qti-assessment-item>`
+    );
+  });
+
+  it('should convert CDATA sections to comments', async () => {
+    const parsedXML = qtiTransformItem()
+      .parse(
+        xml`
+<qti-assessment-item>
+  <qti-item-body>
+    <qti-custom-operator class="js.org">
+      <qti-base-value><![CDATA[some CDATA content]]></qti-base-value>
+    </qti-custom-operator>
+  </qti-item-body>
+</qti-assessment-item>`
+      )
+      .convertCDATAtoComment()
       .html();
 
     expect(parsedXML).toMatch(
       html`
 <qti-assessment-item xmlns="http://www.w3.org/1999/xhtml">
+  <qti-item-body>
+    <qti-custom-operator class="js.org">
+      <qti-base-value><!--some CDATA content--></qti-base-value>
+    </qti-custom-operator>
+  </qti-item-body>
+</qti-assessment-item>`
+    );
+  });
+
+  it('should strip style sheets from the document', async () => {
+    const parsedXML = qtiTransformItem()
+      .parse(
+        xml`
+        <qti-assessment-item>
+          <qti-stylesheet href="style.css" />
+          <qti-item-body>
+            <qti-choice-interaction response-identifier="RESPONSE">
+              <qti-simple-choice identifier="A">Choice A</qti-simple-choice>
+              <qti-simple-choice identifier="B">Choice B</qti-simple-choice>
+            </qti-choice-interaction>
+          </qti-item-body>
+        </qti-assessment-item>`
+      )
+      .stripStyleSheets()
+      .html();
+
+    expect(parsedXML).toEqualXml(
+      html`
+      <qti-assessment-item xmlns="http://www.w3.org/1999/xhtml">
         <qti-item-body>
-          <qti-choice-interaction-custom response-identifier="RESPONSE" class="type:custom">
+          <qti-choice-interaction response-identifier="RESPONSE">
             <qti-simple-choice identifier="A">Choice A</qti-simple-choice>
             <qti-simple-choice identifier="B">Choice B</qti-simple-choice>
-          </qti-choice-interaction-custom>
+          </qti-choice-interaction>
         </qti-item-body>
-    </qti-assessment-item>`
+      </qti-assessment-item>`
+    );
+  });
+
+  it('should update elements with pciHooks correctly', async () => {
+    const parsedXML = qtiTransformItem()
+      .parse(
+        xml`
+        <qti-assessment-item>
+          <qti-item-body>
+            <qti-custom-interaction hook="customHook" module="customModule" />
+          </qti-item-body>
+        </qti-assessment-item>`
+      )
+      .pciHooks('https://example.com/path')
+      .html();
+
+    expect(parsedXML).toEqualXml(
+      html`
+      <qti-assessment-item xmlns="http://www.w3.org/1999/xhtml">
+        <qti-item-body>
+          <qti-custom-interaction hook="customHook" module="https://example.com/customHook.js" base-url="https://example.com/path"></qti-custom-interaction>
+        </qti-item-body>
+      </qti-assessment-item>`
+    );
+  });
+
+  it('should extend element names with specified suffix correctly', async () => {
+    const parsedXML = qtiTransformItem()
+      .parse(
+        xml`
+        <qti-assessment-item>
+          <qti-item-body>
+            <qti-choice-interaction response-identifier="RESPONSE">
+              <qti-simple-choice identifier="A">Choice A</qti-simple-choice>
+              <qti-simple-choice identifier="B">Choice B</qti-simple-choice>
+            </qti-choice-interaction>
+          </qti-item-body>
+        </qti-assessment-item>`
+      )
+      .extendElementName('qti-choice-interaction', 'extended')
+      .html();
+
+    expect(parsedXML).toEqualXml(
+      html`
+      <qti-assessment-item xmlns="http://www.w3.org/1999/xhtml">
+        <qti-item-body>
+          <qti-choice-interaction-extended response-identifier="RESPONSE">
+            <qti-simple-choice identifier="A">Choice A</qti-simple-choice>
+            <qti-simple-choice identifier="B">Choice B</qti-simple-choice>
+          </qti-choice-interaction-extended>
+        </qti-item-body>
+      </qti-assessment-item>`
     );
   });
 });
