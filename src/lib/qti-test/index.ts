e9e1b808f953176422d79599188bc4a33a0305da
Patrick de Klein <patrick.deklein@cito.nl>
2024-12-09 13:25:21 +0100

    refactor: Remove unused exports in index.ts

--- src/lib/qti-test/index.ts
+++ src/lib/qti-test/index.ts
@@ -1,6 +1,3 @@
-export * from './test-base';
-export * from './components';
-export * from './context';
-export * from './mixins';
 export * from './qti-assessment-test';
 export * from './qti-test';
+export * from './components';
