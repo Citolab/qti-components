51626b28e14861bc9e295c7ad541c8d5a2f2e266
Patrick de Klein <patrick.deklein@cito.nl>
2024-12-17 14:26:05 +0100

    chore: Apply inline styles to qti-slider-interaction component

--- src/lib/qti-components/qti-interaction/qti-slider-interaction/qti-slider-interaction.stories.ts
+++ src/lib/qti-components/qti-interaction/qti-slider-interaction/qti-slider-interaction.stories.ts
@@ -1,50 +1,22 @@
 import { html } from 'lit';
-import { action } from '@storybook/addon-actions';
+import { QtiSliderInteraction } from './qti-slider-interaction';
+import { StoryObj, Meta } from '@storybook/web-components';
+import { getWcStorybookHelpers } from 'wc-storybook-helpers';
 
-import './qti-slider-interaction';
-import { ifDefined } from 'lit/directives/if-defined.js';
+const { events, args, argTypes, template } = getWcStorybookHelpers('qti-slider-interaction');
 
-import { Meta } from '@storybook/web-components';
+type Story = StoryObj<QtiSliderInteraction & typeof args>;
 
-const meta: Meta = {
+const meta: Meta<QtiSliderInteraction> = {
   component: 'qti-slider-interaction',
-  argTypes: {
-    orientation: {
-      control: { type: 'radio' },
-      options: ['horizontal', 'vertical'],
-      table: { category: 'QTI' }
-    },
-    readonly: { control: { type: 'boolean' } },
-    disabled: { control: { type: 'boolean' } },
-    step: { control: { type: 'number' } }
+  args,
+  argTypes,
+  parameters: {
+    actions: {
+      handles: events
+    }
   }
 };
 export default meta;
 
-export const Default = {
-  render: args => {
-    const { min, max, step, orientation, stepLabel, reverse, response, readonly, disabled } = args;
-    return html`
-      <!-- style="width:300px; left:100px; position: absolute" -->
-      <qti-slider-interaction
-        @qti-interaction-response="${action(`qti-interaction-response`)}"
-        ?disabled=${disabled}
-        ?readonly=${readonly}
-        response=${ifDefined(response)}
-        response-identifier="RESPONSE"
-        lower-bound=${min ? ifDefined(min) : 0}
-        upper-bound=${max ? ifDefined(max) : 10}
-        step=${ifDefined(step)}
-        orientation=${ifDefined(orientation)}
-        ?step-label=${stepLabel}
-        ?reverse=${reverse}
-      >
-        <qti-prompt>
-          In total, what percentage of the UK population do you think were eventually classified as having no religion?
-        </qti-prompt>
-      </qti-slider-interaction>
-    `;
-  },
-
-  args: {}
-};
+export const Default = (args: any) => html` ${template(args)} `;
---
+++ src/lib/qti-components/qti-interaction/qti-slider-interaction/qti-slider-interaction.styles.ts
@@ -0,0 +1,71 @@
+import { css } from 'lit';
+
+export default css`
+  :host {
+    display: block;
+    --show-bounds: true;
+    --show-ticks: true;
+    --show-value: true;
+  }
+
+  [part='slider'] {
+    margin-left: 2rem; /* mx-8 */
+    margin-right: 2rem;
+    padding-bottom: 1rem; /* pb-4 */
+    padding-top: 1.25rem; /* pt-5 */
+  }
+
+  [part='bounds'] {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    margin-bottom: 0.5rem; /* mb-2 */
+  }
+
+  [part='ticks'] {
+    margin-left: 0.125rem; /* mx-0.5 */
+    margin-right: 0.125rem;
+    margin-bottom: 0.25rem; /* mb-1 */
+    height: 0.5rem; /* h-2 */
+    background: linear-gradient(to right, var(--qti-border-color) var(--qti-border-thickness), transparent 1px) repeat-x
+      0 center / calc(calc(100% - var(--qti-border-thickness)) / ((var(--max) - var(--min)) / var(--step))) 100%;
+  }
+
+  [part='rail'] {
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+    height: 0.375rem; /* h-1.5 */
+    width: 100%;
+    cursor: pointer;
+    border-radius: 9999px; /* rounded-full */
+    border: 1px solid #d1d5db; /* border-gray-300 */
+    background-color: #e5e7eb; /* bg-gray-200 */
+  }
+
+  [part='knob'] {
+    background-color: var(--qti-bg-active);
+    border: 2px solid var(--qti-border-active);
+    position: relative;
+    height: 1rem; /* h-4 */
+    width: 1rem; /* w-4 */
+    transform-origin: center;
+    transform: translateX(-50%);
+    cursor: pointer;
+    border-radius: 9999px; /* rounded-full */
+    left: var(--value-percentage);
+  }
+
+  [part='value'] {
+    position: absolute;
+    bottom: 2rem; /* bottom-8 */
+    left: 0.5rem; /* left-2 */
+    transform: translateX(-50%);
+    cursor: pointer;
+    border-radius: 0.25rem; /* rounded */
+    background-color: #f3f4f6; /* bg-gray-100 */
+    padding: 0.25rem 0.5rem; /* px-2 py-1 */
+    text-align: center;
+    color: #6b7280; /* text-gray-500 */
+  }
+`;
