fcdf122fc0e86bcf83faf3afe49693717ba2dec1
Patrick de Klein <patrick.deklein@cito.nl>
2024-12-17 13:20:56 +0100

    chore: Apply inline styles to qti-item component

--- src/lib/qti-item/qti-item.ts
+++ src/lib/qti-item/qti-item.ts
@@ -1,9 +1,18 @@
 import { LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { QtiItemMixin } from './qti-item.mixin';
+import itemCss from '../../item.css?inline';
 
 @customElement('qti-item')
-export class QtiItem extends QtiItemMixin(LitElement) {}
+export class QtiItem extends QtiItemMixin(LitElement) {
+  connectedCallback() {
+    super.connectedCallback();
+    // Dynamically create and apply styles
+    const sheet = new CSSStyleSheet();
+    sheet.replaceSync(itemCss);
+    this.shadowRoot.adoptedStyleSheets = [sheet];
+  }
+}
 
 declare global {
   interface HTMLElementTagNameMap {
