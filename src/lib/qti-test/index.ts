714eb85172ec15208a0ff964e11805d15c920425
Patrick de Klein <patrick.deklein@cito.nl>
2025-01-09 14:30:11 +0100

    ci: removed mixins and testbase export

--- src/lib/qti-test/index.ts
+++ src/lib/qti-test/index.ts
@@ -2,6 +2,3 @@ export * from './qti-test';
 export * from './qti-assessment-test';
 export * from './components';
 export * from './context';
-
-// export * from './mixins';
-// export * from './test-base';
