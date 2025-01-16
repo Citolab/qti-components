68cb30184811c0574664c0e4dd741e68b2f9fa28
Marcel Hoekstra <marcel_hoekstra@outlook.com>
2025-01-16 15:40:29 +0100

    fix: update identifier in ITEM002.xml and remove auto-score-items attribute in test.stories.ts

--- src/stories/test.stories.ts
+++ src/stories/test.stories.ts
@@ -25,7 +25,7 @@ type Story = StoryObj;
 export const Test: Story = {
   render: (_, { loaded: { testURL } }) => {
     return html`
-      <qti-test auto-score-items class="flex h-full overflow-hidden">
+      <qti-test class="flex h-full overflow-hidden">
         <test-paging-buttons-stamp class="flex flex-col gap-2 p-2 overflow-auto" style="min-width:160px">
           <template>
             <test-item-link item-id="{{ item.identifier }}"> {{ item.identifier }} </test-item-link>
