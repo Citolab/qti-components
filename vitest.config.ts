71dc005f1ad3c05136c708188963ebe05f30f033
Patrick de Klein <patrick.deklein@cito.nl>
2025-03-07 12:32:58 +0100

    test: include test-coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
     include: ['src/**/*.spec.ts', 'src/**/*.test.ts'],
     globals: true,
     coverage: {
-      include: ['src/lib/component-editor/**/*.ts'],
       provider: 'v8' // or 'v8'
     },
     browser: {
@@ -29,15 +28,5 @@ export default defineConfig({
     onConsoleLog(log: string, type: 'stdout' | 'stderr'): boolean | void {
       return !log.includes('Lit is in dev mode');
     }
-  },
-  server: {
-    fs: {
-      allow: ['public/assets'] // Allows serving static files from 'public'
-    },
-    open: false,
-    strictPort: true
-  },
-  define: {
-    'process.env.PUBLIC_URL': JSON.stringify('/public')
   }
 });
