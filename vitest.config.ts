599f1caf828e16332ac16067c794ce0c7c2e3d9b
Patrick de Klein <patrick.deklein@cito.nl>
2025-03-07 11:57:26 +0100

    chore: update storybook and vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,16 +7,24 @@ export default defineConfig({
     dangerouslyIgnoreUnhandledErrors: true,
     include: ['src/**/*.spec.ts', 'src/**/*.test.ts'],
     globals: true,
+    coverage: {
+      include: ['src/lib/component-editor/**/*.ts'],
+      provider: 'v8' // or 'v8'
+    },
     browser: {
       enabled: true,
-      name: 'chromium',
       provider: 'playwright',
       headless: true, // Both modes work fine
-      providerOptions: {
-        launch: {
-          args: ['--remote-debugging-port=9222']
+      instances: [
+        {
+          browser: 'chromium',
+          provide: {
+            launch: {
+              args: ['--remote-debugging-port=9222']
+            }
+          }
         }
-      }
+      ]
     },
     onConsoleLog(log: string, type: 'stdout' | 'stderr'): boolean | void {
       return !log.includes('Lit is in dev mode');
