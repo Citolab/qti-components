a80628a2ebce71606111f5a62117720417ba4006
Patrick de Klein <patrick.deklein@cito.nl>
2025-03-18 14:47:01 +0100

    chore: added story for qti-assessment-item

--- src/lib/qti-components/qti-response-processing/qti-rule/qti-lookup-outcome-value/qti-lookup-outcome-value.ts
+++ src/lib/qti-components/qti-response-processing/qti-rule/qti-lookup-outcome-value/qti-lookup-outcome-value.ts
@@ -1,10 +1,13 @@
-import { property } from 'lit/decorators.js';
+import { property, state } from 'lit/decorators.js';
+import { consume } from '@lit/context';
 
 import { convertNumberToUniversalFormat } from '../../../internal/utils';
 import { QtiRule } from '../qti-rule';
+import { itemContext } from '../../../../exports/qti-assessment-item.context';
 
 import type { QtiExpression } from '../../../../exports/qti-expression';
 import type { OutcomeVariable } from '../../../../exports/variables';
+import type { ItemContext } from '../../../../exports/item.context';
 
 /**
  * The lookupOutcomeValue rule sets the value of an outcome variable to the value obtained
@@ -14,13 +17,20 @@ import type { OutcomeVariable } from '../../../../exports/variables';
 export class QtiLookupOutcomeValue extends QtiRule {
   @property({ type: String }) identifier: string;
 
+  @consume({ context: itemContext, subscribe: true })
+  @state()
+  protected context?: ItemContext;
+
   get childExpression(): QtiExpression<string> {
     return this.firstElementChild as QtiExpression<string>;
   }
 
   public override process(): number {
     const identifier = this.getAttribute('identifier');
-    const outcomeVariable = this.closest('qti-assessment-item').getVariable(identifier) as OutcomeVariable;
+
+    const outcomeVariable: OutcomeVariable | null =
+      this.context.variables.find(v => v.identifier === identifier) || null;
+
     let value;
     if (outcomeVariable.interpolationTable) {
       value = outcomeVariable.interpolationTable.get(parseInt(this.childExpression.calculate()));
