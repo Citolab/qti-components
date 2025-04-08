bf4d36436613f98f8ec7e17f74a155d3fdb6fac6
Patrick de Klein <patrick.deklein@cito.nl>
2025-04-08 13:46:31 +0200

    feat: test view toggle works with stampino now

--- src/lib/qti-test/components/test-view-toggle.ts
+++ src/lib/qti-test/components/test-view-toggle.ts
@@ -1,24 +1,35 @@
 import { consume } from '@lit/context';
-import { html, LitElement } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { html, LitElement, nothing } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import { prepareTemplate } from 'stampino';
 
 import { sessionContext } from '../../exports/session.context';
 
+import type { PropertyValues } from 'lit';
 import type { SessionContext } from '../../exports/session.context';
 
 @customElement('test-view-toggle')
 export class TestViewToggle extends LitElement {
+  myTemplate: any;
+
   protected createRenderRoot(): HTMLElement | DocumentFragment {
     return this;
   }
-
-  @property({ type: String, reflect: true, attribute: 'class-for-input' })
-  classForInput: string;
 
   @consume({ context: sessionContext, subscribe: true })
   private sessionContext: SessionContext;
 
   viewOptions = ['candidate', 'scorer', ''];
+
+  connectedCallback(): void {
+    super.connectedCallback();
+    const templateElement = this.querySelector<HTMLTemplateElement>('template');
+    if (!templateElement) {
+      this.myTemplate = null;
+      return;
+    }
+    this.myTemplate = prepareTemplate(templateElement);
+  }
 
   _switchView(view: string) {
     this.dispatchEvent(
@@ -30,30 +41,22 @@ export class TestViewToggle extends LitElement {
     );
   }
 
+  protected firstUpdated(_changedProperties: PropertyValues): void {
+    this.addEventListener('click', () => {
+      if (this.sessionContext?.view === 'scorer') {
+        this._switchView('candidate');
+      } else {
+        this._switchView('scorer');
+      }
+    });
+  }
+
   render() {
-    return html`
-      <label for="viewToggle" part="label">
-        <input
-          type="checkbox"
-          role="switch"
-          aria-checked=${this.sessionContext?.view === 'scorer'}
-          id="viewToggle"
-          class=${`${this.classForInput}`}
-          ?checked=${this.sessionContext?.view === 'scorer'}
-          @change=${(e: Event) => {
-            const el = e.target as HTMLInputElement;
-            if (el.checked) {
-              this._switchView('scorer');
-            } else {
-              this._switchView('candidate');
-            }
-          }}
-        />
-        <!-- for switch -->
-        <!-- <span class="toggle" part="toggle"></span> -->
-        <slot></slot>
-      </label>
-    `;
+    return html`${this.myTemplate
+      ? this.myTemplate({
+          view: this.sessionContext?.view
+        })
+      : nothing}`;
   }
 }
 
