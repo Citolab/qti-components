5a164e2b92b17b6d578ae0d681b206a7f0032d41
Denise Broekman <denise.broekman@cito.nl>
2026-03-19 15:29:19 +0100

    chore(release): condense release run logs

--- packages/qti-theme/.releaserc.cjs
+++ packages/qti-theme/.releaserc.cjs
@@ -12,7 +12,7 @@ module.exports = {
       '@semantic-release/git',
       {
         assets: ['CHANGELOG.md', 'package.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        message: 'chore(release): ${nextRelease.version} [skip ci]'
       }
     ],
     ...(hasGithubToken ? ['@semantic-release/github'] : [])
