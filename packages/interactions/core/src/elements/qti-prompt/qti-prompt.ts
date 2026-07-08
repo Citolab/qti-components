2ab39a3fae6677c4ecde291fbe001e884c517994
Patrick de Klein <patrick.deklein@cito.nl>
2026-07-08 09:54:08 +0200

    fix: add optional chaining to prevent errors when accessing parentElement in QtiPrompt

--- packages/interactions/core/src/elements/qti-prompt/qti-prompt.ts
+++ packages/interactions/core/src/elements/qti-prompt/qti-prompt.ts
@@ -12,10 +12,11 @@ export class QtiPrompt extends LitElement {
   }
 
   override connectedCallback(): void {
+    super.connectedCallback();
     // if prompts are in interactions they should have a slot, so the prompt has to go there
     // if prompt is in the body, then just display the prompt there.
     // A better check would be the latter, but not can't get through the shadowroot to find the slot
-    const inInteraction = this.parentElement.tagName.endsWith('INTERACTION');
+    const inInteraction = this.parentElement?.tagName.endsWith('INTERACTION');
     if (inInteraction) {
       this.setAttribute('slot', 'prompt');
     }
