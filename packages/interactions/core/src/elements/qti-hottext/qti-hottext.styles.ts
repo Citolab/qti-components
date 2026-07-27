925bdf140610cdb9b0e11b3448970efcccd25f84
Patrick de Klein <patrick.deklein@cito.nl>
2026-07-27 12:31:56 +0200

    style: update Kennisnet theme variables and mixins for improved consistency and appearance

--- packages/interactions/core/src/elements/qti-hottext/qti-hottext.styles.ts
+++ packages/interactions/core/src/elements/qti-hottext/qti-hottext.styles.ts
@@ -11,7 +11,6 @@ export default [
       display: inline-flex;
       align-items: center;
       position: relative;
-      margin: var(--qti-inline-margin, 0);
       padding: var(--qti-inline-padding, 1px 4px);
     }
 
